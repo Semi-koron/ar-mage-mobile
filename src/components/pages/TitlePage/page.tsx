6723974cb5b5bc8067aaa756461282931019814b
Semi-koron <6jimoto6@gmail.com>
2026-08-25 14:07:57 +0900

    fix:スタイルを一部修正

--- src/components/pages/TitlePage/page.tsx
+++ src/components/pages/TitlePage/page.tsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
+import styles from "./index.module.css";
 import Button from "../../feature/Button";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { NavLink } from "react-router-dom";
 
 function TitlePage() {
   const [roomCode, setRoomCode] = useState("");
 
   return (
-    <>
-      <h1>AR Mage Camera</h1>
-      <p>Please input your room code:</p>
-      <input
-        type="text"
-        placeholder="Room code"
-        value={roomCode}
-        onChange={(e) => setRoomCode(e.target.value)}
-      />
-      <NavLink to={`/game/${roomCode}`}>
-        <Button onClick={() => console.log(roomCode)}>Join Game</Button>
-      </NavLink>
-    </>
+    <div className={styles["title-page-wrapper"]}>
+      <div className={styles["title-page-container"]}>
+        <div className={styles["title-text"]}>
+          <h1>AR Maze Camera</h1>
+          <img
+            src="/maze.svg"
+            alt="MazeLogo"
+            className={styles["title-image"]}
+          />
+        </div>
+        <p>ルームのコードを入れてね</p>
+        <input
+          type="text"
+          placeholder="Room code"
+          value={roomCode}
+          onChange={(e) => setRoomCode(e.target.value)}
+          className={styles["input-style"]}
+        />
+        <NavLink to={`/game/${roomCode}`}>
+          <Button onClick={() => console.log(roomCode)}>
+            Join Game
+            <ArrowForwardIcon className={styles["arrow-icon"]} />
+          </Button>
+        </NavLink>
+      </div>
+    </div>
   );
 }
 
